1561a889a7d3a32a5bf19ba43cfa961d974dc00f
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-14 11:32:11 -0400

    algo 03 solved and unsolved

--- 03-Algorithms/03-camel-case/Solved/camel-case.js
+++ 03-Algorithms/03-camel-case/Solved/camel-case.js
@@ -1,19 +1,52 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
+// var camelCase = function(str) {
+//   var result = "";
+//   var words = str.toLowerCase().split(" ");
+
+//   for (var i = 0; i < words.length; i++) {
+//     var word = words[i];
+//     var letters = word.split("");
+
+//     if (i > 0) {
+//       letters[0] = letters[0].toUpperCase();
+//     }
+
+//     result += letters.join("");
+//   }
+
+//   return result;
+// };
+
 var camelCase = function(str) {
   var result = "";
-  var words = str.toLowerCase().split(" ");
+  // var words = str.toLowerCase().split(" ");
+  var splitWordsArr = str.toLowerCase().split(" ")
+ ;
 
-  for (var i = 0; i < words.length; i++) {
-    var word = words[i];
-    var letters = word.split("");
+  for (var i = 0; i < splitWordsArr.length; i++) {
+    var singleWordArr = splitWordsArr[i].split("");
+    console.log(singleWordArr);
 
-    if (i > 0) {
-      letters[0] = letters[0].toUpperCase();
-    }
+    singleWordArr[0] = singleWordArr[0].toUpperCase();
+    console.log(singleWordArr);
 
-    result += letters.join("");
+    result = result + singleWordArr.join("");
+    console.log(result);
+
+    
+    // singleWord = singleWord[i].toUpperCase + singleWord.subString(2);
+    // var word = words[i];
+    // var letters = word.split("");
+
+    // if (i > 0) {
+    //   letters[0] = letters[0].toUpperCase();
+    // }
+
+    // result += letters.join("");
   }
 
-  return result;
+  console.log(result);
 };
+
+camelCase("How are you")
--- 03-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ 03-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = (str) => {
+  let splitArr = str.split(" "); // ["Myyyy" "name" "is" "Arpita" "kar"]
+
+  for (i = 0; i < splitArr.length; i++) {
+    splitArr[i] = splitArr[i][0].toUpperCase() + splitArr[i].substring(1).toLowerCase();
+  }
+  let strJoin = splitArr.join("");
+  let finalCamelCase = strJoin[0].toLowerCase() + strJoin.substring(1);
+  console.log(finalCamelCase);
+};
+
+camelCase("Myyyy name is Arpita kar");
